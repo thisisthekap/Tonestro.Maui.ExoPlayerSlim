b82b807505f31229c365a4919cbc69d3b7776ac0
thisisthekap <christian.kapplmueller@tonestro.com>
2023-07-03 15:40:22 +0200

    pushed net7 android bindings

--- Tonestro.Maui.ExoPlayerSlim.UsageChecker/MainActivity.cs
+++ Tonestro.Maui.ExoPlayerSlim.UsageChecker/MainActivity.cs
@@ -1,13 +1,30 @@
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Com.Tonestro.Exoplayerslim;
+
 namespace Tonestro.Maui.ExoPlayerSlim.UsageChecker;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
 public class MainActivity : Activity
 {
+    private View _videoView;
+    private IExoPlayerSlim _exoPlayerSlim;
+
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
+        _videoView = ExoPlayerSlimViewFactory.Create(this);
+        _exoPlayerSlim = ExoPlayerSlimFactory.Create(this, "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8");
+
+        _exoPlayerSlim.AttachPlayerView(_videoView,true,AspectRatios.Fit);
+
+        _exoPlayerSlim.PlayWhenReady = true;
+
         // Set our view from the "main" layout resource
-        SetContentView(Resource.Layout.activity_main);
+        SetContentView(_videoView);
+
+        _exoPlayerSlim.Play();
     }
 }
