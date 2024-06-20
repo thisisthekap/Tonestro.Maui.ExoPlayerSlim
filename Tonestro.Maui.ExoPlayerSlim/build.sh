2e6a4fd8a14db15b93f30bf30ecf2f745b4e70d8
thisisthekap <christian.kapplmueller@tonestro.com>
2024-06-20 16:06:01 +0200

    removed build.sh

--- Tonestro.Maui.ExoPlayerSlim/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo dotnet workload restore
-dotnet build -t:InstallAndroidDependencies -f net8.0-android -p:AcceptAndroidSDKLicenses=True -p:JavaSdkDirectory="/Users/ckapplmueller/Downloads/kappijava" -p:AndroidSdkDirectory="/Users/ckapplmueller/Downloads/kappiandroid"
-dotnet build -c Release -p:JavaSdkDirectory="/Users/ckapplmueller/Downloads/kappijava" -p:AndroidSdkDirectory="/Users/ckapplmueller/Downloads/kappiandroid"
-dotnet pack -c Release -p:JavaSdkDirectory="/Users/ckapplmueller/Downloads/kappijava" -p:AndroidSdkDirectory="/Users/ckapplmueller/Downloads/kappiandroid"
